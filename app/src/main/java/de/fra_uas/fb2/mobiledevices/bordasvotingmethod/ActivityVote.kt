60c3954c1b84bf6352f50acd2e8f84ff725c048b
Padamadam <arybojad@gmail.com>
2024-05-11 10:41:56 +0200

    Added buttons changing activity view

--- app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethod/ActivityVote.kt
+++ app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethod/ActivityVote.kt
@@ -2,33 +2,34 @@ package de.fra_uas.fb2.mobiledevices.bordasvotingmethod
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
+class ActivityVote : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_vote)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val addVoteButton: Button = findViewById<Button>(R.id.addVoteButton);
-        addVoteButton.setOnClickListener{
-            val intent = Intent(this, ActivityVote::class.java)
+        val cancelButton: Button = findViewById<Button>(R.id.cancelButton);
+        cancelButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-
+        val confirmVoteButton: Button = findViewById<Button>(R.id.confirmVoteButton);
+        confirmVoteButton.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
-
 }
